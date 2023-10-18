f5291e6982a03f805903e3450f1c553d7d4e6e00
jhon65496 <jhon65496@yandex.ru>
2023-10-18 13:58:20 +0300

    ch

---
+++ WpfControlLib02/AppManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WpfControlLib02
+{
+    public class AppManager
+    {   
+        public AppManager()
+        {
+     
+        }
+    }
+}
---
+++ WpfControlLib0202/AppManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlLib0202
+{
+    public class AppManager
+    {
+
+        
+        public AppManager()
+        {
+            
+        }
+
+
+       
+
+    }
+}
